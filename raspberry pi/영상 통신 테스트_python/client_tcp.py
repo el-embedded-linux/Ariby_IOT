00df0872ca2f170936d72207831e25ae23d2ddc2
LeeSunR <sd844@naver.com>
2019-01-02 07:30:50 +0900

    영상 통신 테스트

---
+++ raspberry pi/영상 통신 테스트_python/client_tcp.py
@@ -0,0 +1,36 @@
+import cv2
+import io
+import socket
+import struct
+import time
+import pickle
+import zlib
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('192.168.219.109', 8485))
+connection = client_socket.makefile('wb')
+
+cam = cv2.VideoCapture(2)
+
+cam.set(3, 320);
+cam.set(4, 240);
+
+img_counter = 0
+
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow('ImageWindow',frame)
+    cv2.waitKey(1)
+    frame = cv2.resize(frame, dsize=(320, 240), interpolation=cv2.INTER_AREA)
+    result, frame = cv2.imencode('.jpg', frame, encode_param)
+#    data = zlib.compress(pickle.dumps(frame, 0))
+    data = pickle.dumps(frame, 0)
+    size = len(data)
+
+    print("{}: {}".format(img_counter, size))
+    client_socket.sendall(struct.pack(">L", size) + data)
+    img_counter += 1
+
+cam.release()
