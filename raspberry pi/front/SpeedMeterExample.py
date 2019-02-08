f2044fba253e8cb5613243290b2e0fca2d9129fd
LeeSunR <sd844@naver.com>
2019-02-08 13:34:06 +0900

    테스트용 쓰레드 추가

--- raspberry pi/front/SpeedMeterExample.py
+++ raspberry pi/front/SpeedMeterExample.py
@@ -1,10 +1,10 @@
-import blecomm
+import SpeedMeter
 
-# speedmeterStart 의 첫번째 인자로 블루투스 디바이스의 주소를 입력
-# 두번째 인자로는 콜백 함수를 등록해줌
-# 블루투스로부터 스피드값이 올때마다 콜백 함수가 실행됨
-
-def getSpeed(data):
+def SpeedUpdate(data):
     print("speed : "+data)
 
-blecomm.speedmeterStart('F0:45:DA:10:B9:C1',getSpeed)
+speedmeter = SpeedMeter.SpeedMeter('F0:45:DA:10:B9:C1',SpeedUpdate)
+speedmeter.start_b()
+
+while True:
+    pass
