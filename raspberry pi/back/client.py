029dd73f1929fca41a66394e5f32652a56e50609
LeeSunR <sd844@naver.com>
2019-02-09 12:51:10 +0900

    클라이언트 서버 통신

--- raspberry pi/back/client.py
+++ raspberry pi/back/client.py
@@ -7,22 +7,17 @@ import pickle
 import zlib
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 8485))
+client_socket.connect(('127.0.01', 8485))
 connection = client_socket.makefile('wb')
 
 cam = cv2.VideoCapture(0)
-
-cam.set(3, 160);
-cam.set(4, 90);
-
 img_counter = 0
-
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
 while True:
     ret, frame = cam.read()
     result, frame = cv2.imencode('.jpg', frame, encode_param)
-#    data = zlib.compress(pickle.dumps(frame, 0))
+    data = zlib.compress(pickle.dumps(frame, 0))
     data = pickle.dumps(frame, 0)
     size = len(data)
 
