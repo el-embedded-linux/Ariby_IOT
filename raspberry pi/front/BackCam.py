93621574086931d2be6057542034a2255c8e3a65
LeeSunR <sd844@naver.com>
2019-02-13 14:41:13 +0900

    udp통신

--- raspberry pi/front/BackCam.py
+++ raspberry pi/front/BackCam.py
@@ -1,6 +1,9 @@
 import cv2
 import threading
 from PyQt5.QtGui import *
+import socket
+import struct
+import pickle
 
 class BackCam():
     isStoped = True
@@ -8,6 +11,8 @@ class BackCam():
     #initializer
     def __init__(self, frameUpdate):
         self.frameUpdate = frameUpdate
+        sock = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+        sock.bind( ('127.0.0.1',8080) )
 
     #thread start
     def start(self):
@@ -19,19 +24,14 @@ class BackCam():
 
     def run(self):
         self.isStoped = False
-        cap = cv2.VideoCapture(-1) # -1 or 0
-        cap.set(3,640)
-        cap.set(4,360)
         while True:
-            ret, frame = cap.read()
-            if ret:
-                frame = cv2.resize(frame, dsize=(800, 480), interpolation=cv2.INTER_AREA) #라즈베리파이 스크린 사이즈에 맞게 RESIZE
-                image = QImage(frame, frame.shape[1], frame.shape[0], frame.shape[1] * 3,QImage.Format_RGB888) #create QIamge
-                pix = QPixmap(image) #create QPixmap
-                self.frameUpdate(pix) #callback frameUpdate
-
-            else:
-                break
+            data , addr = sock.recvfrom(65535)
+            frame=pickle.loads(data, fix_imports=True, encoding="bytes")
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+            frame = cv2.resize(frame, dsize=(800, 480), interpolation=cv2.INTER_AREA) #라즈베리파이 스크린 사이즈에 맞게 RESIZE
+            image = QImage(frame, frame.shape[1], frame.shape[0], frame.shape[1] * 3,QImage.Format_RGB888) #create QIamge
+            pix = QPixmap(image) #create QPixmap
+            self.frameUpdate(pix) #callback frameUpdate
 
             if self.isStoped: #flag check
                 break
