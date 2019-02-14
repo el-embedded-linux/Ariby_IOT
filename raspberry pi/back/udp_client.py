f5ba15a34aa2c529ae4650c2d83d2b35ee9af80c
LeeSunR <sd844@naver.com>
2019-02-15 06:03:40 +0900

    udp로 영상전송

---
+++ raspberry pi/back/udp_client.py
@@ -0,0 +1,24 @@
+import socket
+import cv2
+import struct
+import pickle
+import zlib
+
+sock = socket.socket( socket.AF_INET , socket.SOCK_DGRAM )
+
+cam = cv2.VideoCapture(0)
+img_counter = 0
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+while True:
+    ret, frame = cam.read()
+    result, frame = cv2.imencode('.jpg', frame, encode_param)
+    data = zlib.compress(pickle.dumps(frame, 0))
+    data = pickle.dumps(frame, 0)
+    size = len(data)
+    print(size)
+    #stream = struct.pack(">L", size)+data
+    sock.sendto( data , ('127.0.0.1',8080) )
+
+    print(img_counter)
+    img_counter +=1
