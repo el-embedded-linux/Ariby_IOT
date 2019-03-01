46bf721252b735444a4a7a3bbe1f32fdd2229854
mgjung2018 <46953779+mgjung2018@users.noreply.github.com>
2019-03-02 06:36:00 +0900

    전방서버

---
+++ raspberry pi/front/connServer.py
@@ -0,0 +1,15 @@
+import sys
+import socket
+import MainWindow
+ 
+def runServer() : ## 서버열기
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ## IP4v이기 때문에 AF_INET 사용
+    sock.bind(('localhost', 4001))
+    sock.listen(1)
+
+    conn, addr = sock.accept()
+    data = conn.recv(1024)
+    print(data)
+
+
+
