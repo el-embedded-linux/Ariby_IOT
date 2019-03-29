ad6ac7aca4883ded5e61ce0e13167160eb701371
mgjung2018 <mgjung2013@gmail.com>
2019-03-29 20:29:13 +0900

    handsoff.py 완료

--- raspberry pi/front/handsoff.py
+++ raspberry pi/front/handsoff.py
@@ -1,66 +1,59 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 import threading
 
-class handtouchcheck(threading.Thread):
-    time_stack = 0
-    overtime = 5
-    handon = None
-    handoff = None
+class handcheck(threading.Thread) :
+    time_stack = 0      ## 누적시간
+    overtime = 3          ## 제한시간 3초
+    touchHand = None       ## 터치센서 핀번호
+ 
+    ledPinNumber = None ## LED 핀번호
+    checkLED = False       ## LED on(True),off(False) 확인
 
-    
-    ledPin = None
-
-    touchHand = None
-    
-    checkLED = False
-    isStopped = False
+    isStopped = False     ## handoff 끝내기
 
 
-  
-    def __init__(self, handon, handoff):    
+    def __init__(self, handon, handoff):
         threading.Thread.__init__(self)
-        self.touchHand = 21
-        self.ledPin = 27
+        self.touchHand = 21           ## 터치센서 
+        self.ledPinNumber = 23     ## LED 핀번호
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.touchHand,GPIO.IN)
-        GPIO.setup(self.ledPin,GPIO.OUT)
-        GPIO.output(self.ledPin, False)
+        GPIO.setup(self.touchHand, GPIO.IN)         ## 터치센서 on
+        GPIO.setup(self.ledPinNumber, GPIO.OUT) ## LED 출력설정
+        GPIO.output(self.ledPinNumber, GPIO.LOW)
 
         self.handon = handon
         self.handoff = handoff
 
-		def run(self):
-			while True :
-				print(GPIO.input(21))
-			for i in range(0,100):
-				sleep(0.01)
-
-	        	if GPIO.OUT == True :
-					self.time_stack = self.time_stack + 1
-
-				else :
-					GPIO.output(self.ledPin, False)
-
-				if self.checkLED == True :
-					self.handoff()
-					self.checkLED = False
-					self.time_stack = 0
-
-				if self.time_stack >= self.overtime :
-					GPIO.output(self.ledPin, True)
-					self.checkLED = True
-					self.handon() 
-
-				if self.isStopped:
-					break
+    def run(self):
+        while True:
+            print(GPIO.input(self.touchHand))
+            
+            self.time_stack = self.time_stack + 1 ## 손을 떼었을 경우 누적시간 누적
+                 
+            if GPIO.input(self.touchHand) == True:                      ## 손을 다시 붙혔을 경우 누적시간 초기화
+                self.time_stack = 0 
+                self.checkLED = False
+                GPIO.output(self.ledPinNumber, GPIO.LOW) ## 소등
+                
+            elif self.time_stack >= self.overtime: ## 누적시간 초 이상
+                   GPIO.output(self.ledPinNumber, GPIO.HIGH) ## 점등
+                   self.checkLED = True
+                   self.handon()
+                
+        
+            time.sleep(1)   
+                    
+            if self.isStopped:
+                break
 
 def _on():
-    #print("on")
-    pass
-def _off():
-    #print("off")
     pass
 
-handtouch = handtouchcheck(_on,_off)
+def _off():
+    pass
+
+
+handtouch = handcheck(_on, _off)
 handtouch.start()
+
