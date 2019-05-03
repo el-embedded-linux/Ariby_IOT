1faee2ce5e6c294776e99e25b83a311d18a9fab0
LeeSunR <sd844@naver.com>
2019-05-03 21:37:47 +0900

    사물인식 추가

--- raspberry pi/back/udp_client.py
+++ raspberry pi/back/udp_client.py
@@ -6,19 +6,8 @@ import zlib
 
 sock = socket.socket( socket.AF_INET , socket.SOCK_DGRAM )
 
-cam = cv2.VideoCapture(0)
-img_counter = 0
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-
-while True:
-    ret, frame = cam.read()
-    result, frame = cv2.imencode('.jpg', frame, encode_param)
-    data = zlib.compress(pickle.dumps(frame, 0))
-    data = pickle.dumps(frame, 0)
+def sendToFront(data):
     size = len(data)
     print(size)
     #stream = struct.pack(">L", size)+data
     sock.sendto( data , ('192.168.100.1',8080) )
-
-    print(img_counter)
-    img_counter +=1
