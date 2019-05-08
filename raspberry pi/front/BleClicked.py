38197d39d0e604791501a9cc41afb1067d2d45f1
LeeSunR <sd844@naver.com>
2019-05-08 22:04:01 +0900

    ui 수정

--- raspberry pi/front/BleClicked.py
+++ raspberry pi/front/BleClicked.py
@@ -1,5 +1,7 @@
 import blscan
 import threading
+import pexpect
+import time
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
@@ -29,24 +31,24 @@ class BleClicked(QLabel):
         self.list = QListWidget(self)
         self.list.setStyleSheet("color:white;font-size:20px;border:0px;QListWidget::item{border:1px solid red;};")
         self.list.setFixedWidth(550)
-        self.list.setFixedHeight(330)
+        self.list.setFixedHeight(280) #550, 330
 
-        self.quitLabel = QLabel()
-        self.quitLabel.setFixedHeight(60)
-        self.quitLabel.setFixedWidth(760)
-
-        self.quitBtn = QPushButton("QUIT")
+        self.quitBtn = QPushButton("Quit")
         self.quitBtn.setFixedHeight(30)
         self.quitBtn.setFixedWidth(70)
         self.quitBtn.setStyleSheet("font:bold 14px Arial; color:rgb(41,41,41); border:0px; border-radius:5px; background-color:rgb(106,230,197); outline:0px;")
         self.quitBtn.clicked.connect(forBack.changeStack)
 
-        self.quitLayout = QGridLayout()
-        self.quitLayout.setContentsMargins(0,0,0,0)
-        self.quitLayout.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
-        self.quitLayout.addWidget(self.quitBtn)
+        self.ConnBtn = QPushButton("Connect")
+        self.ConnBtn.setFixedHeight(30)
+        self.ConnBtn.setFixedWidth(70)
+        self.ConnBtn.setStyleSheet("font:bold 14px Arial; color:rgb(41,41,41); border:0px; border-radius:5px; background-color:rgb(106,230,197); outline:0px;")
+        self.ConnBtn.clicked.connect(self.connClicked)
 
-        self.quitLabel.setLayout(self.quitLayout)
+        self.quitLayout = QHBoxLayout()
+        self.quitLayout.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.quitLayout.addWidget(self.ConnBtn)
+        self.quitLayout.addWidget(self.quitBtn)
 
         self.rightLayOut = QVBoxLayout()
         self.rightLayOut.addWidget(self.list)
@@ -54,15 +56,38 @@ class BleClicked(QLabel):
         self.layout = QHBoxLayout()
         self.layout.addLayout(self.leftLayOut)
         self.layout.addLayout(self.rightLayOut)
-        self.layout.addWidget(self.quitLabel)
-        self.setLayout(self.layout)
-        self.layout.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
-        self.layout.setContentsMargins(0,0,0,0)
-        self.setStyleSheet("background-color:rgb(41,41,41)")
+        self.backlayout = QVBoxLayout()
+        self.backlayout.addLayout(self.layout)
+        self.backlayout.addLayout(self.quitLayout)
+        self.setLayout(self.backlayout)
+        #self.layout.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        #self.layout.setContentsMargins(0,0,0,0)
+        #self.setStyleSheet("background-color:rgb(41,41,41)")
 
     def showDvice(self, device):
         print(device)
-        self.list.addItem('%s' % device)
+        self.list.addItem('addr : ' + '%s' % device['addr'] + ' name : ' + '%s' % device['name'] )
+
+    def connClicked(self):
+        self.itemSelect()
+        conn = pexpect.spawn("bluetoothctl", echo = False)
+        time.sleep(0.2)
+        conn.send("scan on" + "\n")
+        time.sleep(2)
+        conn.send("scan off" + "\n")
+        time.sleep(0.2)
+        conn.expect("Discovery stopped")
+        conn.send("agent on" + "\n")
+        time.sleep(0.2)
+        conn.expect("Agent registered")
+        conn.send("default-agent" + "\n")
+        time.sleep(0.2)
+        conn.expect("Default agent request successful")
+        conn.send("pair 00:F4:6F:7A:A7:98" + "\n")
+        time.sleep(0.2)
+
+        #conn.expect("00:F4:6F:7A:A7:98")
+        print("aaa")
 
     def checkBoxState(self):
             if self.checkBox1.isChecked() == True:
@@ -73,3 +98,8 @@ class BleClicked(QLabel):
                 devices = blscan.scan.start(self.showDvice,'classic')
             else:
                 blscan.scan.stop()
+
+    def itemSelect(self):
+        selectItem = [device.text() for device in self.list.selectedItems()]
+        tmpStr = selectItem[0]
+        print(tmpStr[7:25])
