127816baa5f79393e2be9c06f7d7a38138f53bf7
LeeSunR <sd844@naver.com>
2019-06-05 19:55:40 +0900

    후방 브레이크등 야간 등화 추가

--- raspberry pi/back/BackLedCntroller.py
+++ raspberry pi/back/BackLedCntroller.py
@@ -13,6 +13,8 @@ class BackLedCntroller():
     led_left_on = False
     led_right_on = False
     led_emergency_on = False
+    led_night_on = False
+    led_break_on = False
 
     #깜빡임 간격
     BLINKTIME = 0.5
@@ -27,10 +29,23 @@ class BackLedCntroller():
 
     #브레이크 제어
     def break_on(self):
+        led_break_on = True
         GPIO.output(self.LED_BREAK, GPIO.HIGH)
 
     def break_off(self):
+        led_break_on = False
         GPIO.output(self.LED_BREAK, GPIO.LOW)
+
+    #브레이크 제어
+    def break_night_on(self):
+        self.turn_off(self.LED_LEFT)
+        t = threading.Thread(target=self.break_night_thread, args=())
+        t.start()
+
+    def break_night_off(self):
+        led_night_on = False
+
+
 
     #깜빡이 제어
     def turn_on(self, oriental):
@@ -94,22 +109,37 @@ class BackLedCntroller():
                 GPIO.output(self.LED_LEFT, GPIO.LOW)
                 break
 
+    def break_night_thread(self):
+        self.led_night_on = True
+        while True:
+            GPIO.output(self.LED_BREAK, GPIO.LOW)
+            time.sleep(0.05)
+            GPIO.output(self.LED_BREAK, GPIO.HIGH)
+            time.sleep(0.05)
+            while led_break_on == True:
+                pass
+            if led_night_on == False:
+                break
+
+
 backLedCntroller = BackLedCntroller()
-while True:
-    text = input()
-    if text == "ron":
-        backLedCntroller.turn_on(backLedCntroller.LED_RIGHT)
-    if text == "lon":
-        backLedCntroller.turn_on(backLedCntroller.LED_LEFT)
-    if text == "roff":
-        backLedCntroller.turn_off(backLedCntroller.LED_RIGHT)
-    if text == "loff":
-        backLedCntroller.turn_off(backLedCntroller.LED_LEFT)
-    if text == "breakon":
-        backLedCntroller.break_on()
-    if text == "breakoff":
-        backLedCntroller.break_off()
-    if text == "emeron":
-        backLedCntroller.emergency_on()
-    if text == "emeroff":
-        backLedCntroller.emergency_off()
+
+if __name__ == "__main__"
+    while True:
+        text = input()
+        if text == "ron":
+            backLedCntroller.turn_on(backLedCntroller.LED_RIGHT)
+        if text == "lon":
+            backLedCntroller.turn_on(backLedCntroller.LED_LEFT)
+        if text == "roff":
+            backLedCntroller.turn_off(backLedCntroller.LED_RIGHT)
+        if text == "loff":
+            backLedCntroller.turn_off(backLedCntroller.LED_LEFT)
+        if text == "breakon":
+            backLedCntroller.break_on()
+        if text == "breakoff":
+            backLedCntroller.break_off()
+        if text == "emeron":
+            backLedCntroller.emergency_on()
+        if text == "emeroff":
+            backLedCntroller.emergency_off()
