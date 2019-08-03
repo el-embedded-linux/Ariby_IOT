951d4f34ca1692e1ff4c49282b06826bdd9b8828
koramkids <koramkids@nate.com>
2019-08-04 01:52:31 +0900

    라이트 버전 테스트 전

--- raspberry pi/front/Ariby_Lite.py
+++ raspberry pi/front/Ariby_Lite.py
@@ -1,99 +1,191 @@
-'''
-1. 버그 수정필요 // 그래프 띄울시 백그라운드로 pyqt위젯이 하나 더 생김
-2. Q스택위젯, 마우스클릭이벤트로 인덱스 번호 찾기
-3. pyqtgraph 백그라운드 표출되는 버그 수정바람
-'''
-
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 import pyqtgraph as pg
 
-class StWidgetForm(QGroupBox):
-    """
-    위젯 베이스 클래스
-    """
-    def __init__(self):
-        QGroupBox.__init__(self)
-        self.x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.y = [0, 34, 21, 40, 45, 50, 30, 20, 25, 10]
-        self.SpeedPlot = pg.plot(self.x, self.y)
-        self.SpeedPlot.setXRange(1, 10)
-        self.SpeedPlot.setYRange(1, 60)
-
-class Widget_1(StWidgetForm):
-    """
-    버튼 그룹
-    """
+class Widget_1(QLabel):
     def __init__(self):
         super(Widget_1, self).__init__()
-        # self.setGeometry(0, 0, 320, 240)
         # 스피드
-        self.SpeedLabel = QLabel("Speed\n10Km/h")
-        self.SpeedLabel.setStyleSheet("color:rgb(255,255,255); font-size:40px; font:bold")
-        self.SpeedLabel.setAlignment(Qt.AlignCenter)
-        self.SpeedLayOut = QVBoxLayout()
+        self.SpeedLabel = QLabel(" "+" 88") #100보다 작은 숫자일 땐 앞에 띄어쓰기 2칸 넣어야함, 큰 숫자일 땐 상관없음
+        self.SpeedLabel.setStyleSheet("color:rgb(255,255,255); font-size:60px; font:bold")
+        self.kmLabel = QLabel("\n\nKm/h")
+        self.kmLabel.setFixedWidth(65)
+        self.kmLabel.setStyleSheet("color:rgb(255,255,255); font-size:14px; font:bold")
+        self.SpeedLayOut = QHBoxLayout()
         self.SpeedLayOut.addWidget(self.SpeedLabel)
+        self.SpeedLayOut.addWidget(self.kmLabel)
 
         # 4칸(그리드레이아웃)
-        self.BPMLabel = QLabel("BPMLabel\n14.0")
-        self.BPMLabel.setStyleSheet("color:rgb(255,255,255); font-size:20px; font:bold")
-        self.BPMLabel.setAlignment(Qt.AlignCenter)
-        self.CalLabel = QLabel("CalLabel\n6.37")
-        self.CalLabel.setStyleSheet("color:rgb(255,255,255); font-size:20px; font:bold")
-        self.CalLabel.setAlignment(Qt.AlignCenter)
-        self.RTimeLabel = QLabel("RTimeLabel\n10:16")
-        self.RTimeLabel.setStyleSheet("color:rgb(255,255,255); font-size:20px; font:bold")
-        self.RTimeLabel.setAlignment(Qt.AlignCenter)
-        self.DISTLabel = QLabel("DISTLabel\n12.54km")
-        self.DISTLabel.setStyleSheet("color:rgb(255,255,255); font-size:20px; font:bold")
-        self.DISTLabel.setAlignment(Qt.AlignCenter)
-
         self.FourInnerLayOut = QGridLayout()
-        self.FourInnerLayOut.addWidget(self.BPMLabel, 0, 0)
-        self.FourInnerLayOut.addWidget(self.CalLabel, 0, 1)
-        self.FourInnerLayOut.addWidget(self.RTimeLabel, 1, 0)
-        self.FourInnerLayOut.addWidget(self.DISTLabel, 1, 1)
+        row = 0; col = 0;
+
+        for i in range(0, 4, 1):
+            if i == 0:
+                text1 = "평균속도"
+                value = " 10"
+                text2 = "\nkm/h"
+            elif i == 1:
+                text1 = "이동거리"
+                value = "110"
+                text2 = "\nkm/h"
+            elif i == 2:
+                text1 = "심박수"
+                value = " 80"
+                text2 = "\nbpm"
+            elif i == 3:
+                text1 = "칼로리"
+                value = "200"
+                text2 = "\nKcal"
+
+            self.avgSpeedLayout = QVBoxLayout()
+            self.avgSpeedLayout.setContentsMargins(0,0,0,0)
+            self.innerLayout = QHBoxLayout()
+            self.innerLayout.setContentsMargins(0,0,0,0)
+            self.outerLabel = QLabel()
+            self.outerLabel.setLayout(self.avgSpeedLayout)
+            self.textLabel1 = QLabel(text1)
+            self.textLabel1.setAlignment(Qt.AlignCenter)
+            self.textLabel1.setStyleSheet("color:white;font:bold;font-size:12px;")
+            self.textLabel2 = QLabel()
+            self.textLabel2.setLayout(self.innerLayout)
+            self.avgSpeed = QLabel(value)
+            self.avgSpeed.setStyleSheet("color:white;font:bold;font-size:26px;")
+            self.km = QLabel(text2)
+            self.km.setStyleSheet("color:white;font:bold;font-size:11px;")
+            self.innerLayout.addWidget(self.avgSpeed)
+            self.innerLayout.addWidget(self.km)
+            self.avgSpeedLayout.addWidget(self.textLabel1)
+            self.avgSpeedLayout.addWidget(self.textLabel2)
+
+            if (col == 2):
+                row = 1; col = 0
+
+            self.FourInnerLayOut.addWidget(self.outerLabel, row, col)
+
+            col += 1
 
         self.layout = QVBoxLayout()
         self.layout.addLayout(self.SpeedLayOut)
         self.layout.addLayout(self.FourInnerLayOut)
         self.setLayout(self.layout)
 
-class Widget_2(StWidgetForm):
+
+class Widget_2(QLabel):
     def __init__(self):
         super(Widget_2, self).__init__()
-        # 스피드
-        self.SpeedLabel = QLabel("Speed\n10Km/h")
-        self.SpeedLabel.setStyleSheet("color:rgb(255,255,255); font-size:35px; font:bold")
-        self.SpeedLabel.setAlignment(Qt.AlignCenter)
-        self.SpeedLayOut = QVBoxLayout()
-        self.SpeedLayOut.addWidget(self.SpeedPlot)
-        self.SpeedLayOut.addWidget(self.SpeedLabel)
+        #그래프
+        self.x = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        self.y = [0, 34, 21, 40, 45, 30, 20, 25, 10]
+        self.graphWd = pg.GraphicsWindow()
+        self.graphWd.ci.layout.setContentsMargins(0,0,0,0)
+        self.gp = self.graphWd.addPlot()
+        self.gp.plot(self.x, self.y)
 
-        # 그리드레이아웃
-        self.MaxLabel = QLabel("MaxLabel\n80 km/h")
-        self.MaxLabel.setStyleSheet("color:rgb(255,255,255); font-size:18px; font:bold")
-        self.MaxLabel.setAlignment(Qt.AlignCenter)
-        self.AvgLabel = QLabel("AvgLabel\n55 km/h")
-        self.AvgLabel.setStyleSheet("color:rgb(255,255,255); font-size:18px; font:bold")
-        self.AvgLabel.setAlignment(Qt.AlignCenter)
+        self.gpLabel = QLabel()
+        self.graphLayout = QVBoxLayout()
+        self.graphLayout.setContentsMargins(0,0,0,0)
+        self.graphLayout.addWidget(self.graphWd)
+        self.gpLabel.setLayout(self.graphLayout)
+        self.gpLabel.setFixedHeight(137)
+        self.gpLabel.setFixedWidth(200)
 
         self.FourInnerLayOut = QGridLayout()
-        self.FourInnerLayOut.addWidget(self.MaxLabel, 0, 0)
-        self.FourInnerLayOut.addWidget(self.AvgLabel, 0, 1)
+        row = 0; col = 0;
 
-        self.layout = QVBoxLayout()
-        self.layout.addLayout(self.SpeedLayOut)
-        self.layout.addLayout(self.FourInnerLayOut)
-        self.setLayout(self.layout)
+        for i in range(0, 4, 1): #띄어쓰기 없애지 말기...
+            if i == 0:
+                text1 = "최고속도"
+                value = " "+"35" #값이 100 미만일 경우 / 100 이상 시 " " 제거 필요
+                text2 = "\nkm/h"
+                flag = 0
+            elif i == 1:
+                text1 = "주행시간"
+                value = " "+"08:58"
+                text2 = ""
+                flag =1
+            elif i == 2:
+                text1 = "경사도"
+                value = "  "+"4º"
+                text2 = ""
+                flag = 1
+            elif i == 3:
+                text1 = "온도"
+                value = "  "+"25℃"
+                text2 = ""
+                flag = 1
+
+            self.avgSpeedLayout = QVBoxLayout()
+            self.avgSpeedLayout.setContentsMargins(0,0,0,0)
+            self.innerLayout = QHBoxLayout()
+            self.innerLayout.setContentsMargins(0,0,0,0)
+            self.outerLabel = QLabel()
+            self.outerLabel.setLayout(self.avgSpeedLayout)
+            self.textLabel1 = QLabel(text1)
+            self.textLabel1.setAlignment(Qt.AlignCenter)
+            self.textLabel1.setStyleSheet("color:white;font:bold;font-size:12px;")
+            self.textLabel2 = QLabel()
+            self.textLabel2.setLayout(self.innerLayout)
+            self.avgSpeed = QLabel(value)
+            self.avgSpeed.setStyleSheet("color:white;font:bold;font-size:26px;")
+            self.km = QLabel(text2)
+            self.km.setStyleSheet("color:white;font:bold;font-size:11px;")
+            self.innerLayout.addWidget(self.avgSpeed)
+            if flag == 1:
+                self.innerLayout.setAlignment(Qt.AlignCenter)
+            self.innerLayout.addWidget(self.km)
+            self.avgSpeedLayout.addWidget(self.textLabel1)
+            self.avgSpeedLayout.addWidget(self.textLabel2)
+
+            if (col == 2):
+                row = 1; col = 0
+
+            self.FourInnerLayOut.addWidget(self.outerLabel, row, col)
+
+            col += 1
+
+        self.FourLabel = QLabel()
+        self.FourLabel.setLayout(self.FourInnerLayOut)
+
+        self.layout2 = QVBoxLayout()
+        self.layout2.setContentsMargins(0,0,0,0)
+        self.layout2.addWidget(self.gpLabel)
+        self.layout2.addWidget(self.FourLabel)
+        self.setLayout(self.layout2)
+
+
+class Widget_3(QLabel):
+    def __init__(self):
+        super(Widget_3, self).__init__()
+        self.textLabel = QLabel("주행 중입니다.")
+        self.textLabel.setStyleSheet("font:bold;font-size:12px;color:white;")
+        self.textLabel.setAlignment(Qt.AlignBottom | Qt.AlignCenter)
+        self.textLabel.setFixedHeight(30)
+        self.label = QLabel()
+        self.label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        img = QPixmap('Images/user0.png')
+        self.label.setPixmap(img)
+        self.label.setFixedHeight(250)
+        self.backLabel = QLabel()
+
+        self.layout3 = QVBoxLayout()
+        self.layout3.setContentsMargins(0,0,0,0)
+        self.layout3.addWidget(self.textLabel)
+        self.layout3.addWidget(self.label)
+        self.setLayout(self.layout3)
+
+    def backWarning(self, data): #콜백 등록 필요
+        self.textLabel.setText("후방에 주의하세요.")
+        self.label.setPixmap(QPixmap('Images/user'+str(data)+'.png'))
+
 
 class Form(QWidget):
     def __init__(self):
         QWidget.__init__(self, flags=Qt.Widget)
         self.stk_w = QStackedWidget(self)
         self.init_widget()
-        self.checkIndex = True
+        self.checkIndex = 0
 
     def init_widget(self):
         self.setWindowTitle("Ariby Lite")
@@ -101,17 +193,22 @@ class Form(QWidget):
 
         self.stk_w.addWidget(Widget_1())
         self.stk_w.addWidget(Widget_2())
+        self.stk_w.addWidget(Widget_3())
 
         widget_laytout.addWidget(self.stk_w)
         self.setLayout(widget_laytout)
 
     def mousePressEvent(self, e):
-        if self.checkIndex == False:
-            self.stk_w.setCurrentIndex(0)
-            self.checkIndex = True
-        elif self.checkIndex == True:
+        if self.checkIndex == 1:
+            self.stk_w.setCurrentIndex(2)
+            self.checkIndex = 2
+        elif self.checkIndex == 0:
             self.stk_w.setCurrentIndex(1)
-            self.checkIndex = False
+            self.checkIndex = 1
+        elif self.checkIndex == 2:
+            self.stk_w.setCurrentIndex(0)
+            self.checkIndex = 0
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
