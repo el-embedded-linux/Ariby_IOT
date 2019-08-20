040b60a9138c7d1999908a8d7475150d2dec5fc0
LeeSunR <sd844@naver.com>
2019-08-20 11:21:33 +0900

    아두이노 센서제어 수정

--- arduino_blue/arduino_blue.ino
+++ arduino_blue/arduino_blue.ino
@@ -1,16 +1,31 @@
 #include <MsTimer2.h>
 #define CIRCLE 1.946
-#define REEDSWITCH 2 //리드스위치 포트
+#define REEDSWITCH 7 //리드스위치 포트
+#define SWITCH 6
+#define REGISTER A0
 
 float mySpeed = 0;
-float beforeSpeed = 1;
 uint32_t uckTime = 0;
 uint32_t ckTime = 0;
 uint32_t count = 0;
+uint8_t switch_on = 0;
+
+void notify(){
+  Serial.print(mySpeed,2);
+  Serial.print(",");
+  int reg_origin = analogRead(REGISTER) - 101;
+  int reg_map = map(reg_origin, 0, 1023, 0, 180);
+  Serial.print(reg_map);
+  Serial.print(",");
+  Serial.println(digitalRead(SWITCH));
+}
 
 void setup() {
   Serial.begin(115200);
+  pinMode(SWITCH, INPUT_PULLUP);
   pinMode(REEDSWITCH, INPUT);
+  MsTimer2::set(50,notify);
+  MsTimer2::start();
 }
 
 void loop() {
@@ -20,21 +35,16 @@ void loop() {
   else count = 0;
 
   if(count==10) {
-    
       uckTime = ckTime;
       ckTime = millis();
       uint32_t cycleTime = (ckTime - uckTime);
-      if(cycleTime==0 || uckTime==0) mySpeed = 0;
+      if(cycleTime==0) mySpeed = 0;
       else mySpeed = (CIRCLE/cycleTime) * 3600;
   }
 
   if(count>200000) mySpeed = 0;
+  
+}
 
-  if(mySpeed!=beforeSpeed){
-    beforeSpeed=mySpeed;
-    Serial.print(mySpeed,2);  
-  }
-  if(beforeSpeed==0&&count%20000==0&&count!=0){
-    Serial.print("0.00");
-  }
+void noise_clear(){
 }
--- master/master.ino
+++ master/master.ino
@@ -1,26 +1,25 @@
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(10, 11); // RX, TX
-uint16_t rotate = 0;
+
 
 void setup()  
 {
+  pinMode(2, INPUT_PULLUP);
   Serial.begin(115200);
   mySerial.begin(115200);
 }
 
 void loop() // run over and over
 {
-  uint16_t rotate_temp = analogRead(A0);
+  int dyValue = analogRead(A1);
+  int dyMap = map(dyValue, 0, 1023, 0, 180);
+
+  int swValue = digitalRead(2);
+
+  Serial.print(",");
+  Serial.print(dyMap);
+  Serial.print(",");
+  Serial.println(swValue);
   
-  if(rotate_temp!=rotate) {
-    rotate = rotate_temp;
-    Serial.print("S");
-    Serial.print("rotation=");
-    Serial.print(rotate);
-    Serial.print("E");
-  }
-  while (mySerial.available()){
-    Serial.write(mySerial.read());
-  }
   delay(50);
 }
